9b55da06982a276ab8e9725b8082c784ae10f0a1
Bellar <gbellar@bethelks.edu>
2023-03-02 12:45:24 -0600

    Create stub route handling functions

--- server.py
+++ server.py
@@ -1,11 +1,29 @@
 from flask import *
 
-app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+def sign_up():
+    # for account registration
+    pass
+
+
+def login():
+    # for logging in users
+    pass
+
+
+def submit_tracking_request():
+    # for user to submit their own cities and prices to be tracked
+    pass
+
+
+def view_user_submission_data():
+    # allows users to look at they prices and cities they've entered. Perhaps with editing ability
+    pass
 
 
 if __name__ == "__main__":
