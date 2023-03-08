81ce0c11a8ae4b68478ffa48edc0ddb44c2be21a
gbku12 <gbku12@gmail.com>
2023-03-08 12:26:39 -0600

    Create flight tracking database as child of User
    This creates a many to one relationship between the User data base and FlightTrack, with User being the parent

--- database_setup.py
+++ database_setup.py
@@ -1,11 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 db = SQLAlchemy()
 
+Base = declarative_base()
 
-class User(db.Model):
+
+class User(db.Model, Base):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+
+    flights = relationship("FlightTrack", back_populates='user')
+
+
+
+class FlightTrack(db.Model, Base):
+    __tablename__ = 'tracking'
+    id = db.Column(db.Integer, primary_key=True)
+    departing = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates='flights')
+    
