5b1260c31e6a78f3d75235b0868a6cbd691f34bd
gbku12 <gbku12@gmail.com>
2023-03-15 11:08:22 -0500

    Replace depricated db method with preferred method as of 2.0

--- server.py
+++ server.py
@@ -28,7 +28,8 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return db.session.get(User, user_id)
+    
 
 
 @app.route("/")
